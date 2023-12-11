fa5530b5454ba5a53e333a37e5def8439c126bc2
sajjad <sajjad@pop-os.localdomain>
2023-12-11 11:48:11 +0600

    seven times

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'luminous_sajjad/campaign_sajjad',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0931ad1fef1127cdffc8ceb481b6b3882bc7420f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'luminous_sajjad/campaign_sajjad' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0931ad1fef1127cdffc8ceb481b6b3882bc7420f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
